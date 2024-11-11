62fcfad2937229a90001c6dee5931107977ac319
Alper Sonmez <aziaatyt@gmail.com>
2024-11-11 17:13:32 +0100

    Register Services for ItemService

--- Cargohub/Program.cs
+++ Cargohub/Program.cs
@@ -1,21 +1,22 @@
+using Cargohub.Data;
+using Cargohub.Services;
 using Cargohub.Models;
 using Microsoft.EntityFrameworkCore;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
-
+// Register the database context
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=CargoHub.db"));
 
+// Register services
+builder.Services.AddScoped<IItemService, ItemService>();
+
+// Add controllers
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
 app.Urls.Add("http://localhost:5000");
-
 app.MapControllers();
-
-app.MapGet("/", () => "Hello World!");
-
 app.Run();

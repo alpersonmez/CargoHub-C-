7d56d0770c9048bbedfdb908317adf7508b54c79
Joost <116558339+jostitost1@users.noreply.github.com>
2024-11-11 13:28:41 +0100

    folders

--- Cargohub/Program.cs
+++ Cargohub/Program.cs
@@ -1,5 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+
 var app = builder.Build();
+
+app.Urls.Add("http://localhost:5000");
+
+app.MapControllers();
 
 app.MapGet("/", () => "Hello World!");
 

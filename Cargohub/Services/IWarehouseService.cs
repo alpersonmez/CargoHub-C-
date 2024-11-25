87ac508db3878d29805498900d66c3ceb08628b1
IvarsCode <1057848@hr.nl>
2024-11-25 14:22:06 +0100

    warehouse post fixed

---
+++ Cargohub/Services/IWarehouseService.cs
@@ -0,0 +1,10 @@
+using Cargohub.Models;
+
+namespace Cargohub.Services;
+
+public interface IWarehouseService
+{
+    public Warehouse? GetWarehouse(int id);
+    public List<Warehouse> GetAllWarehouses();
+    public Warehouse? AddWarehouse(Warehouse warehouse);
+}

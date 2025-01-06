166253dcada0d4ef9ca0c17fe313c92ca42835c7
Joost <116558339+jostitost1@users.noreply.github.com>
2025-01-06 18:33:04 +0100

    unit test warehouses

--- Cargohub/Services/IWarehouseService.cs
+++ Cargohub/Services/IWarehouseService.cs
@@ -4,10 +4,9 @@ namespace Cargohub.Services;
 
 public interface IWarehouseService
 {
-    Task<List<Warehouse>> GetAllWareHouses();
-    Task<Warehouse> GetWareHouseById(int id);
+    Task<List<Warehouse>> GetAllWarehouses(int amount = 100);
+    Task<Warehouse> GetWarehouseById(int id);
     Task<Warehouse> AddWarehouse(Warehouse warehouse);
-    //Task<bool> UpdateWareHouse(Warehouse warehouse);
-    //moet nog ff kijken hoe ik dit ga doen
+    Task<bool> UpdateWarehouse(Warehouse warehouse);
     Task<bool> DeleteWarehouse(int id);
 }
